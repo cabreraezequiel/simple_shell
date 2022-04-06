12d1c7d4fdff909a58e6d321768ae476c4a234b3
nicobelen <nicolasbelen93@gmail.com>
2022-04-06 10:16:45 -0700

    fallas en history.c

--- peque/neat/history.c
+++ peque/neat/history.c
@@ -4,35 +4,30 @@ void print_history(char *filename)
 {
 	char *buf;
 	size_t size;
-	int f, i = 0;
-	unsigned int n = 1;
-	char *tok[1024];
+	int f, i = 0, n;
+	char **tok;
 	struct stat st;
+	
+	f = open(filename, O_RDONLY, 0400);
 
 	stat(filename, &st);
 	size = st.st_size;
 
-	f = open(filename, O_RDONLY, 0400);
-
 	buf = malloc(sizeof(char) * size);
 	read(f, buf, size);
 
-/*	tok = malloc(1);
- */
-	while (buf[i] != '\0')
+	tok = malloc(1);
+
+	for (n = 0; n != EOF; n++)
 	{
 		for (i = 0; buf[i] != '\n'; i++)
 		{
 		}
-		n++;
-/*		tok = realloc(tok, (n) * sizeof(char *));
+		tok = realloc(tok, (n) * sizeof(char *));
 		tok[n] = malloc(sizeof(char) * i);
-*/
 	}
 	i = 0;
 	printf("n: %u\n", n);
-
-	read(f, buf, size);
 
 	tok[i] = strtok(buf, "\n");
 
