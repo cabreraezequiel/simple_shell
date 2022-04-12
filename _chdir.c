21a0a76b1bde580a0e66286a5c0ffac8b3ccca94
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-04-12 07:11:19 -0700

    Fixed cd

--- _chdir.c
+++ _chdir.c
@@ -11,15 +11,15 @@ int _chdir(char **path)
 	char s[100];
 
 	s[0] = 0;
+	setenv("PWD", getcwd(s, 100), 1);
 
 /*	if (!path[2])
 		return (1);
 */
-	setenv("OLDPWD", getcwd(s, 100), 1);
-
 	if (path[1] == NULL || (_strcmp(path[1], "$HOME")) == 0)
 	{
 		chdir(getenv("HOME"));
+		setenv("OLDPWD", getenv("PWD"), 1);
 		setenv("PWD", getcwd(s, 100), 1);
 		return (0);
 	}
@@ -27,6 +27,7 @@ int _chdir(char **path)
 	else if ((_strcmp(path[1], "-") == 0))
 	{
 		chdir(getenv("OLDPWD"));
+		setenv("OLDPWD", getenv("PWD"), 1);
 		setenv("PWD", getcwd(s, 100), 1);
 		printf("%s\n", getcwd(s, 100));
 		return (0);
@@ -37,7 +38,6 @@ int _chdir(char **path)
 		if (chdir(path[1]) == -1)
 			fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", path[1]);
 
-		setenv("PWD", getcwd(s, 100), 1);
 		return (0);
 	}
 }
